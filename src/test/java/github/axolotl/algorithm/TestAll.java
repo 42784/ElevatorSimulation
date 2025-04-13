d98fe8c1a799a5679c6f825ade2997af9d6701c9
42784XM <3485376023@qq.com>
2025-04-13 16:37:41 +0800

    更新测试类

--- src/test/java/github/axolotl/algorithm/TestAll.java
+++ src/test/java/github/axolotl/algorithm/TestAll.java
@@ -9,23 +9,32 @@ import org.junit.jupiter.api.Test;
  */
 public class TestAll {
     @Test
-    public void testAll() {
-        github.axolotl.model.TestModel.modelA(new Algorithm_FCFS(),"modelA-FDSC");
-        github.axolotl.model.TestModel.modelB(new Algorithm_FCFS(),"modelB-FDSC");
+    public void testFCFS() {
+        github.axolotl.model.TestModel.modelA(new Algorithm_FCFS(), "modelA-FCFS");
+        github.axolotl.model.TestModel.modelB(new Algorithm_FCFS(), "modelB-FCFS");
+    }
 
-        github.axolotl.model.TestModel.modelA(new Algorithm_LOOK(),"modelA-LOOK");
-        github.axolotl.model.TestModel.modelB(new Algorithm_LOOK(),"modelB-LOOK");
+    @Test
+    public void testLOOK() {
+        github.axolotl.model.TestModel.modelA(new Algorithm_LOOK(), "modelA-LOOK");
+        github.axolotl.model.TestModel.modelB(new Algorithm_LOOK(), "modelB-LOOK");
+    }
 
-        github.axolotl.model.TestModel.modelA(new Algorithm_FDSCAN(),"modelA-FDSCAN");
-        github.axolotl.model.TestModel.modelB(new Algorithm_FDSCAN(),"modelB-FDSCAN");
+    @Test
+    public void testFDSCAN() {
+        github.axolotl.model.TestModel.modelA(new Algorithm_FDSCAN(), "modelA-FDSCAN");
+        github.axolotl.model.TestModel.modelB(new Algorithm_FDSCAN(), "modelB-FDSCAN");
+    }
 
-        github.axolotl.model.TestModel.modelA(new Algorithm_SSTF(),"modelA-SSTF");
-        github.axolotl.model.TestModel.modelB(new Algorithm_SSTF(),"modelB-SSTF");
+    @Test
+    public void testSSTF() {
+        github.axolotl.model.TestModel.modelA(new Algorithm_SSTF(), "modelA-SSTF");
+        github.axolotl.model.TestModel.modelB(new Algorithm_SSTF(), "modelB-SSTF");
+    }
 
-        github.axolotl.model.TestModel.modelA(new Algorithm_SCAN(),"modelA-SCAN");
-        github.axolotl.model.TestModel.modelB(new Algorithm_SCAN(),"modelB-SCAN");
-
-//        github.axolotl.model.TestModel.modelC(new Algorithm_FCFS(),"modelC-FDSCAN");
-//        github.axolotl.model.TestModel.modelD(new Algorithm_FCFS(),"modelD-FDSCAN");
+    @Test
+    public void testSCAN() {
+        github.axolotl.model.TestModel.modelA(new Algorithm_SCAN(), "modelA-SCAN");
+        github.axolotl.model.TestModel.modelB(new Algorithm_SCAN(), "modelB-SCAN");
     }
 }
